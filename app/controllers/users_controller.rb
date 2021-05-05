2406f126425bacea99aa8ca734cf2c92d8aae4e4
Gruinch <sel.chemi86@gmail.com>
2021-05-05 07:08:15 +0000

    edit user_show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+
   def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
   def edit
+    @user = USer.find(params[:id])
   end
 
   def update
