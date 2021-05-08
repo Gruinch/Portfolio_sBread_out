6980fe867bc3dd2a306e43516bd452d0509401b8
Gruinch <sel.chemi86@gmail.com>
2021-05-08 03:08:23 +0000

    edit mypage, edit profile page layaouts

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'd2ad4b39ef01ac9bed8c8c626b6121527ec23907d14fe544a0c9dd5731ace67cf2b31ac26437e34b1ebe4158f466179b6d6f839e62cfe7e34bea879f5c271c84'
