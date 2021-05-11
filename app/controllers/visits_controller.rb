5c8890d51f9808b0b8b6e3c21b547cbcc5ba6acb
Gruinch <sel.chemi86@gmail.com>
2021-05-11 13:03:23 +0000

    Add Visit Button

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,14 @@
 class VisitsController < ApplicationController
+
   def create
+    @article = Article.find(params[:article_id])
+    @visit = current_user.visits.new(article_id: params[:article_id])
+    @visit.save
   end
 
   def destroy
+    @article = Article.find(params[:article_id])
+    @visit = Visit.find_by(user_id: current_user.id, article_id: @article.id)
+    @visit.destroy
   end
 end
