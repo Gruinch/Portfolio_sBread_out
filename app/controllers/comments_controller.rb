a9ad8d2f631c3516f589eea344487efddebe9532
Gruinch <sel.chemi86@gmail.com>
2021-05-30 13:22:25 +0000

    Add maxlength to comment-form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,11 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    article = Article.find(params[:article_id])
+    @article = Article.find(params[:article_id])
     comment = current_user.comments.new(comment_params)
-    comment.article_id = article.id
+    comment.article_id = @article.id
     comment.save
-    redirect_to article_path(article)
+    redirect_to article_path(@article)
   end
 
   def destroy
